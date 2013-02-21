5834c6d078f190ec9d2519b96877cedb6296c9f9
masuda <masuda@L3600>
2013-02-21 12:19:33 +0900

    add debug code

--- server/src/main/java/open/dolphin/rest/KarteResource.java
+++ server/src/main/java/open/dolphin/rest/KarteResource.java
@@ -34,13 +34,17 @@ public class KarteResource extends AbstractResource {
     @Produces(MEDIATYPE_JSON_UTF8)
     public String getKarte(@PathParam("ptId") Long patientPK, 
             @QueryParam("fromDate") String fromDateStr) {
-
+        
+        LapTimer timer = new LapTimer("KarteResource.getKarte");
+        
         Date fromDate = parseDate(fromDateStr);
 
         KarteBean karte = karteServiceBean.getKarte(patientPK, fromDate);
 
         String json = getConverter().toJson(karte);
         debug(json);
+        
+        timer.printLapTime();
         
         return json;
     }
@@ -56,6 +60,8 @@ public class KarteResource extends AbstractResource {
             @QueryParam("toDate") String toDateStr, 
             @QueryParam("includeModified") Boolean includeModified) {
 
+        LapTimer timer = new LapTimer("KarteResource.getDocumentList");
+        
         Date fromDate = parseDate(fromDateStr);
         Date toDate = parseDate(toDateStr);
 
@@ -63,7 +69,9 @@ public class KarteResource extends AbstractResource {
 
         String json = getConverter().toJson(result);
         debug(json);
-
+        
+        timer.printLapTime();
+        
         return json;
     }
 //katoh$
