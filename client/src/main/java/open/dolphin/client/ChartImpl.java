4d383f444d6aff84f5f45338964e0932d97fd635
MasudaNaika <peu605@yahoo.co.jp>
2013-02-27 15:22:20 +0900

    add debug code

--- client/src/main/java/open/dolphin/client/ChartImpl.java
+++ client/src/main/java/open/dolphin/client/ChartImpl.java
@@ -28,6 +28,7 @@ import open.dolphin.project.Project;
 import open.dolphin.setting.MiscSettingPanel;
 import open.dolphin.util.AgeCalculator;
 import open.dolphin.util.GUIDGenerator;
+import open.dolphin.util.LapTimer;
 import open.dolphin.util.MMLDate;
 import org.apache.commons.lang.time.DurationFormatUtils;
 import org.apache.log4j.Level;
@@ -289,8 +290,8 @@ public class ChartImpl extends AbstractMainTool implements Chart, IInfoModel {
     @Override
     public void start() {
         
+        final LapTimer timer = new LapTimer();
         
-
         final SimpleWorker worker = new SimpleWorker<KarteBean, Void>() {
             
             private int periodComboIndex;
@@ -311,6 +312,7 @@ public class ChartImpl extends AbstractMainTool implements Chart, IInfoModel {
                 today.clear(Calendar.MILLISECOND);
                 
 //masuda^       // KarteBeanを取得
+                timer.lap("Getting KarteBean");
                 DocumentDelegater ddl = DocumentDelegater.getInstance();
                 long patientId = getPatientVisit().getPatientModel().getId();
                 KarteBean karteBean = ddl.getKarte(patientId, today.getTime());
@@ -327,6 +329,7 @@ public class ChartImpl extends AbstractMainTool implements Chart, IInfoModel {
                 ExtractionPeriod period = DocumentHistory.EXTRACTION_OBJECTS[periodComboIndex];
                 
                 // DocInfoModelは別に取得する
+                timer.lap("Getting DocInfoModels");
                 DocumentSearchSpec spec = new DocumentSearchSpec();
                 spec.setKarteId(karteBean.getId());                 // カルテID
                 spec.setDocType(IInfoModel.DOCTYPE_KARTE);          // 文書タイプ
@@ -351,18 +354,14 @@ public class ChartImpl extends AbstractMainTool implements Chart, IInfoModel {
                 karteBean.setPatientModel(getPatientVisit().getPatientModel());
                 setKarte(karteBean);
                 //-------------------------------------------------------------
+                timer.lap("InitComponents");
                 initComponents();
-                // もたもた？
-                //SwingUtilities.invokeLater(new Runnable() {
-
-                    //@Override
-                    //public void run() {
-                        getDocumentHistory().showHistory();
+                timer.lap("ShowHistory");
+                getDocumentHistory().showHistory();
 //masuda^   抽出期間コンボ設定・ブロック解除
-                        getDocumentHistory().setExtractionPeriodComboIndex(periodComboIndex);
+                getDocumentHistory().setExtractionPeriodComboIndex(periodComboIndex);
 //masuda$
-                    //}
-                //});
+                timer.lap("Finish succeeded");
             }
 
             @Override
@@ -384,10 +383,12 @@ public class ChartImpl extends AbstractMainTool implements Chart, IInfoModel {
 
             @Override
             protected void stopProgress() {
+                timer.lap("StopProgress");
                 taskTimer.stop();
                 monitor.close();
                 taskTimer = null;
                 monitor = null;
+                timer.stop();
             }
         };
 
--- client/src/main/java/open/dolphin/util/LapTimer.java
+++ client/src/main/java/open/dolphin/util/LapTimer.java
@@ -9,13 +9,24 @@ package open.dolphin.util;
 public class LapTimer {
 
     private long sTime;
-
-    public  void start() {
+    private StringBuilder sb;
+    
+    public LapTimer() {
         sTime = System.currentTimeMillis();
+        sb = new StringBuilder();
+        sb.append("Lap timer started at ").append(sTime);
     }
 
-    public void stop(String msg) {
-        long eTime = System.currentTimeMillis();
-        System.out.println(msg + " in msec :" + String.valueOf(eTime - sTime));
+    public void lap(String msg) {
+        long t = System.currentTimeMillis();
+        sb.append(msg).append(" at ").append(t);
+        sb.append(" (").append(t - sTime).append(")\n");
+    }
+    
+    public void stop() {
+        long t = System.currentTimeMillis();
+        if (t - sTime > 2000 || true) {
+            System.out.println(sb.toString());
+        }
     }
 }
